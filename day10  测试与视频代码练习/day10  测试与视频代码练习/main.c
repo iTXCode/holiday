9fecc5487bd0a45a3979e13ee283c17d9d843ec2
jack-wang128801 <1632672310@qq.com>
2019-02-10 22:56:02 +0800

    今天主要是边看书《c语言深度解剖》边运行一些实验代码，将数中提出的一些问题进行了一些验证。在《c和指针》中看了一下指针哪一章。感觉看一遍就有一边的收获。

---
+++ day10  测试与视频代码练习/day10  测试与视频代码练习/main.c
@@ -0,0 +1,52 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+//#include<stdio.h>
+//#include<windows.h>
+//
+//int main(){
+//
+//	/*int i = 0;
+//	for (i = 0, printf("First=%d\n", i);\
+//		i<=10, printf("Second=%d\n", i);\
+//		i++, printf("Thrid=%d\n", i))
+//	{
+//		printf("Fourth=%d\n",i);
+//	}*/
+//	int i = 3;
+//	/*i++, printf("i=%d\n", i);
+//	++i, printf("i=%d\n", i);*/
+//	/*i=(++i) + (++i) + (++i) ;*/ 
+//	i = 2 % (-2);
+//	printf("i=%d\n", i);
+//	system("pause");
+//	return 0;
+//}
+
+
+//#include<stdio.h>
+//#include<windows.h>
+//
+//#define X 3
+//#define Y X*2
+//#undef  X
+//#define X 2
+//int main(){
+//	int z = Y;
+//	printf("%d\n",z);
+//	system("pause");
+//	return 0;
+//}
+
+
+//#include<stdio.h>
+//#include<windows.h>
+//
+//int main(){
+//	int a = 3;
+//	float b = 3.14;
+//	int *p = &a;
+//	float *q = &b;
+//	printf("%d\n", a);
+//	system("pause");
+//	return 0;
+//}
